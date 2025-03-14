48a8fff439a98fa89717a98f4f2cbdfb4a41e058
Ngô Ngọc Triệu Mẫn <meomeochitchithihi@gmail.com>
2025-03-15 04:58:01 +0700

    bosung va chinh sua menu game

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -4,10 +4,23 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
+    public GameObject uiShop;
     public void PlayGame(){
         SceneManager.LoadSceneAsync(1);
+
     }
     public void ExitGame(){
         Application.Quit();
     }
+
+    public void ShowUIShop()
+    {
+        uiShop.SetActive(true);
+    }
+
+    public void HideUIShop()
+    {
+        uiShop.SetActive(false);
+    }
+
 }
